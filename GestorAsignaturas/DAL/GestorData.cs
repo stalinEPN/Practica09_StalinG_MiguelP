6b342b4e67354a6235a216b0926633ac1e22c849
M147D <david.rmig@gmail.com>
2024-07-03 08:36:06 -0500

    Creación de la capa de acceso a datos

---
+++ GestorAsignaturas/DAL/GestorData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+/*
+ * Se incluye la libreria para trabajar con entity framework
+ */
+using System.Data.Entity;
+/*
+ * Se incluye la referencia hacia el modelo
+ */
+using GestorAsignaturas.Models;
+
+namespace GestorAsignaturas.DAL
+{
+    /*
+     * Se deriva la clase DbConxtext
+     */
+    public class GestorData : DbContext
+    {
+        /*
+         * Se incluye la propiedad que almacenara el conjunto de objetos de tipo aignatura
+         */
+        public DbSet<Asignatura> Asignaturas { get; set; }
+    }
+}
